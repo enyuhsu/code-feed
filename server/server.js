1452909bca27dd436411190014ef5a095c88bfad
RedWood <mackey91@gmail.com>
2015-08-06 12:05:25 -0700

    started routing

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
 
-
+var sequelize = new Sequelize('postgres://localhost/codefeed');
 // middleware
 app.use(express.static('client'));
 
 
+app.post('/post', function (req, res) {
+  Post.save().then(function (err, post) {
 
+  });
+});
 
 app.listen(3000);
