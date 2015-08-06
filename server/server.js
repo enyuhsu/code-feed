5d0fb67eb84f688b8dc3025cd039f060639d710b
Ken Kaiser <kenkaiser@Kens-MacBook-Pro.local>
2015-08-06 11:59:57 -0700

    database setup

--- server/server.js
+++ server/server.js
@@ -1,9 +1,73 @@
 var express = require('express');
 var app = express();
+var Sequelize = require('sequelize');
+
+
+
+
+
+
+var sequelize = new Sequelize('postgres://localhost/codefeed');
+
+var User = sequelize.define('user', {
+	username: {
+		type: Sequelize.STRING,
+		field: 'username'
+	},
+	password: {
+		type: Sequelize.STRING,
+		field: 'password'
+	},
+	usertoken: {
+		type: Sequelize.STRING,
+		field: 'usertoken'
+	},
+	email: {
+		type: Sequelize.STRING,
+		field: 'email'
+	}
+	}, {
+	freezeTableName: true
+	
+});
+
+var Post =  sequelize.define('post',{
+	title: {
+		type: Sequelize.STRING,
+		field: 'title'
+	},
+	url: {
+		type: Sequelize.STRING,
+		field: 'url'
+	},	
+	posts: {
+		type: Sequelize.STRING,
+		field: 'posts'
+	}
+    },{
+	freezeTableName: true
+});
+
+User.hasMany(Post, {as: 'posts'});
+
+
+
+Post.sync({force:true}).then(function(){
+	return Post.create({
+		title: 'BOBs',
+		url: 'donut.com',
+		posts: "these donut are yummy!"
+	});
+});
+
+
+
 
 
 // middleware
 app.use(express.static('client'));
+
+
 
 
 
