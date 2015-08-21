7c39a85949bff2a8e31b6e29f43ab85d74f9c735
MattEubanks <moosefan94@gmail.com>
2015-08-21 11:08:06 -0700

    database-server-json

--- server/server.js
+++ server/server.js
@@ -8,229 +8,152 @@ var express = require('express'),
     http = require('http'),
     array = [];
 
-mongoose.connect('', function(err){
+mongoose.connect('mongodb://Thlapath:meubanks1@ds059672.mongolab.com:59672/recoddit', function(err){
   if(err){return err;}
+  console.log("connected to Db");
 });
 
 var UserSchema = new Schema({
-	email:{type: , required: , index: {}}
-})
+	email:{type: String , required:true , index: {unique: true}},
+	username:{type: String, required:true, index: {unique: true}},
+	password: {type: String, required: true},
+	access_token: {type: String}
+});
 
+var PostSchema = new Schema({
+	title: {type: String, required: true},
+	url: {type: String, required: true},
+	post: {type: String, required: true},
+	postedBy: {type: mongoose.Schema.Types.ObjectId, ref: 'User' required: true},
+	comment: [{body: "string", by: mongoose.Schema.Types.ObjectId}]//should reference comment id
+}); 
 
+// var CommentSchema = new Schema({
+// 	comment: {type: String, required: true},
+// 	date: {type: date, required: true},
+
+// });
+
+var User = mongoose.model("User", UserSchema);
+var Post = mongoose.model("Post", PostSchema);
 app.use(express.static('client'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+// app.post('/fb_login', function(req,res){
+// 	User
+// 	  .findOrCreate({
+// 	  	where : {
+// 	  		username : req.body.username
+// 	  	},
+// 	  	defaults:{
+// 	  		usertoken : req.body.usertoken
+// 	  	}
+// 	  }).spread(function(user,created){
+// 	  	user.updateAttributes({
+//       		usertoken : req.body.usertoken
+//       	});
+// 	  });
+// });
 
-var User = sequelize.define('users', {
-	username: {
-		type: Sequelize.STRING,
-		field: 'username'
-	},
-	password: {
-		type: Sequelize.STRING,
-		field: 'password'
-	},
-	usertoken: {
-		type: Sequelize.STRING,
-		field: 'usertoken'
-	},
-	email: {
-		type: Sequelize.STRING,
-		field: 'email'
-	}
-	}, {
-	freezeTableName: true
-
-});
-
-var Post =  sequelize.define('post',{
-	title: {
-		type: Sequelize.STRING,
-		field: 'title'
-	},
-	url: {
-		type: Sequelize.STRING,
-		field: 'url'
-	},
-	posts: {
-		type: Sequelize.STRING,
-		field: 'posts'
-	},
-	username: {
-		type : Sequelize.STRING,
-		field: 'username'
-	}
-  },{
-
-	freezeTableName: true
-});
-
-var Upvote =  sequelize.define('upvote',{
-  title: {
-    type: Sequelize.INTEGER,
-    field: 'upvote'
-  }
-  }, {
-
-  freezeTableName: true
-});
-
-
-var Comment = sequelize.define('comment', {
-  comments:{
-    type: Sequelize.STRING,
-    field:'comments'
-  }
-    },{
-   freezeTableName: true
-});
-
-User.sync();
-Post.sync();
-Comment.sync();
-Upvote.sync();
-
-User.hasMany(Post, {as: 'posts'});
-User.hasMany(Comment,{as: 'comments'});
-Post.hasMany(Comment, {as: 'comments'});
-Post.belongsTo(User);
-// Comments.belongsToMany()
-
-
-
-
-
-app.post('/fb_login', function(req,res){
-	User
-	  .findOrCreate({
-	  	where : {
-	  		username : req.body.username
-	  	},
-	  	defaults:{
-	  		usertoken : req.body.usertoken
-	  	}
-	  }).spread(function(user,created){
-	  	user.updateAttributes({
-      		usertoken : req.body.usertoken
-      	});
-	  });
-});
-
-app.get('/user/:id', function (req, res) {
-  User
-    .findById(id)
-    .then(function (user) {
-      res.send(user);
-    })
-    .catch(function (error) {
-      if (error) {
-        res.send(error);
-      }
-    });
-});
+// app.get('/user/:id', function (req, res) {
+//   User
+//     .findById(id)
+//     .then(function (user) {
+//       res.send(user);
+//     })
+//     .catch(function (error) {
+//       if (error) {
+//         res.send(error);
+//       }
+//     });
+// });
 
 app.post('/comment', function (req, res) {
-  console.log(req.body);
-  Comment
-    .create(req.body)
-    .then(function(comment){
-      res.send(comment);
-    })
-    .catch(function (error) {
-      if (error) {
-        res.send(error);
-      }
-    });
+  Post.findOneAndUpdate({_id: req.body.postId}, function(err,post){
+  	if(err) throw err;
+  	post.comment.push({
+  		body: req.body.body,
+  		by: req.body.username
+  	});
+  });
 });
 
 // middleware
 
-app.get('/com', function (req, res) {
-  Comment
-    .findAll()
-    .then(function (comments) {
-      res.send(comments);
-    })
-    .catch(function (error) {
-      res.send(error);
-    });
+app.get('/comments', function (req, res) {
+	Post.find({_id: req.body.postId}, function(err, post){
+		if(err) throw err;
+		res.send(post.comment);
+	});  
 });
 
 app.post('/post', function (req, res) {
+		var newpost = new Post({
+				title: req.body.title,
+				url: req.body.url,
+				post: req.body.post,
+				postedBy: req.body.username
+		});
+		newpost.save(function(){
+			console.log("saved a new post");
+		})
+
+
 	//query database where username = req.body.username and retrieve usertoken
 	//if usertoken !== req.cookies.access_token
-	console.log("Body: "+req.body);
-	if(!req.cookies.username){
-		console.log("Cookies don't exist: "+req.cookies.username);
-		//res.send('Please log in before posting');
-		res.error();
-		res.end();
-	} else {
-		console.log("Cookies exist: "+req.cookies.username);
-		User.find({username: req.cookies.username})
-			.then(function(user){
-				if(!user){
-					res.send('Please log in before posting');
-				}
-				else if(req.cookies.access_token !== user.usertoken){
-					console.log("Invalid token");
-					res.error();
-					res.end();
-				}
-				req.body.username = req.cookies.username;
-				console.log(req.body);
-				Post
-				  .create(req.body)
-				  .then(function(post){
-				  	res.send(post);
-				  	//res.send('post added');
-				  })
-				  .catch(function (error) {
-				    if (error) {
-				      res.send(error);
-				    }
-				  });
-			});
+	// console.log("Body: "+req.body);
+	// if(!req.cookies.username){
+	// 	console.log("Cookies don't exist: "+req.cookies.username);
+	// 	//res.send('Please log in before posting');
+	// 	res.error();
+	// 	res.end();
+	// } else {
+	// 	console.log("Cookies exist: "+req.cookies.username);
+	// 	User.find({username: req.cookies.username})
+	// 		.then(function(user){
+	// 			if(!user){
+	// 				res.send('Please log in before posting');
+	// 			}
+	// 			else if(req.cookies.access_token !== user.usertoken){
+	// 				console.log("Invalid token");
+	// 				res.error();
+	// 				res.end();
+	// 			}
+	// 			req.body.username = req.cookies.username;
+	// 			console.log(req.body);
+	// 			Post
+	// 			  .create(req.body)
+	// 			  .then(function(post){
+	// 			  	res.send(post);
+	// 			  	//res.send('post added');
+	// 			  })
+	// 			  .catch(function (error) {
+	// 			    if (error) {
+	// 			      res.send(error);
+	// 			    }
+	// 			  });
+	// 		});
 
 
-  }
+ //  }
 });
 
 
 app.get('/posts', function (req, res) {
-  Post
-    .findAll()
-    .then(function (posts) {
-      res.send(posts);
-    })
-    .catch(function (error) {
-      res.send(error);
-    });
+  Post.find({}, function(err,posts){
+  	if(err) throw err;
+  	res.send(posts);
+  });
 });
 
-app.get('/fb_users', function (req, res) {
-  User
-    .findAll()
-    .then(function (users) {
-      res.send(users);
-    })
-    .catch(function (error) {
-      res.send(error);
-    });
-});
+// app.get('/fb_users', function (req, res) {
+ 
+// });
 
-app.post('/upvote', function (req, res) {
-  console.log(req.body);
-  Upvote
-    .create(req.body)
-    .then(function(upvote) {
-      res.send(upvote);
-    })
-    .catch(function (error) {
-      res.send(error);
-    });
-});
+// app.post('/upvote', function (req, res) {
+  
+// });
 
 
 app.listen(process.env.PORT || 3000);
